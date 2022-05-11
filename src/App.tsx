e7f47540d81746e15448e4a686ee03e1252f248e
nmacdon3 <natemacd97@gmail.com>
2022-05-11 09:05:29 -0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ interface JobType {
   company: string;
   duration: string;
   description: string;
+  responsibilities: string[];
 }
 
 const jobs: JobType[] = [
@@ -18,19 +19,23 @@ const jobs: JobType[] = [
     title: "Lead Frontend Developer - Lead UX Designer",
     company: "Gray Wolf Analytics Inc.",
     duration: "January 2021 - Present",
-    description: `in the knowledge industry consists of QA, DevOps and
-software engineering roles. Currently, I am the frontend
-developer for Gray Wolf Analytics; a crypto AML solution for
-securing blockchain networ`,
+    description: `Nathan was hired as the sole Frontend contributor to this fast-paced startup right after graduation. The small size of the company allowed him to try new roles and gain new expertise including: `,
+    responsibilities: [
+      "Building, testing, and deploying a production-grade web app",
+      "Designing attractive and intuitive UI / UX",
+      "Shaping company goals into actionable projects",
+    ],
   },
   {
     title: "Software Developer",
     company: "IBM",
     duration: "September 2017 - September 2020",
-    description: `in the knowledge industry consists of QA, DevOps and
-    software engineering roles. Currently, I am the frontend
-    developer for Gray Wolf Analytics; a crypto AML solution for
-    securing blockchain networ`,
+    description: `Nathan worked here 5 terms during his formal education as part of his CO-OP designation. Each term taught him new skills such as:`,
+    responsibilities: [
+      "Performing automated QA on an app's frontend",
+      "Large-scale container orchestration",
+      "Thorough understanding of SCRUM and agile methodologies",
+    ],
   },
 ];
 
@@ -40,6 +45,7 @@ const technicalSkills = [
   "UI/UX Design",
   "CSS",
   "API Development",
+  "Python",
 ];
 const professionalSkills = [
   "Project Planning",
@@ -62,13 +68,8 @@ const Section = ({
   );
 };
 
-const aboutText = `I graduated from University of New Brunswick with a minor in
-Business Administration and CO-OP designation. My work
-experience in the knowledge industry consists of QA, DevOps and
-software engineering roles. Currently, I am the frontend
-developer for Gray Wolf Analytics; a crypto AML solution for
-securing blockchain networks.`;
-
+const aboutText =
+  "Nathan is a passionate, driven, and highly ambitious worker who takes immense pride in the things he creates. He cares deeply about the end user experience, and is dedicated to providing humane and beautiful interfaces for everyday use.";
 const Job = ({ job }: { job: JobType }) => {
   return (
     <div>
@@ -80,43 +81,40 @@ const Job = ({ job }: { job: JobType }) => {
         <BsDot className="h-4 w-4" />
         <span className=""> {job.duration}</span>
       </div>
-      <p className="text-xs text-gray-500 text-justify mt-2">
+      <p className="text-xs text-gray-500 text-justify mt-2 mb-2">
         {job.description}
       </p>
+      {job.responsibilities.map((resp) => (
+        <li className="text-xs text-gray-500 ml-5">{resp}</li>
+      ))}
     </div>
   );
 };
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-gradient-to-t from-gray-300  flex justify-center font-body">
-      <div className="w-[44rem] h-full bg-white relative p-10 shadow-2xl overflow-hidden flex flex-col">
-        <div
-          // style={{ backgroundColor: "#C0A28B" }}
-          className="h-96  bg-orange-300/40 w-[60rem] absolute -top-56 -left-6 rotate-6 overflow-hidden"
-        >
-          <div
-            // style={{ backgroundColor: "#9C9C79" }}
-            className="h-96 bg-orange-300/30 w-[60rem] absolute -top-40 -right-20 rotate-45"
-          ></div>
+    <div className="h-screen w-screen bg-gradient-to-t from-gray-300  flex justify-center items-center font-body">
+      <div className="w-[44rem] h-full bg-white relative px-10 shadow-2xl overflow-hidden flex flex-col">
+        <div className="h-96  bg-orange-300/40 w-[60rem] absolute -top-56 -left-6 rotate-6 overflow-hidden">
+          <div className="h-96 bg-orange-300/30 w-[60rem] absolute -top-40 -right-20 rotate-45"></div>
         </div>
-        <header className="flex items-start space-x-8">
+        <header className="flex items-start space-x-8 shrink-0 pt-10">
           <img
-            className="rounded-full ring-4 ring-white h-28 w-28 z-20 relative object-cover"
+            className="rounded-full ring-4 ring-white h-16 w-16 sm:h-28 sm:w-28 z-20 relative object-cover"
             src={profile}
           />
           <div>
-            <h1 className="text-5xl font-thin relative tracking-wide mb-2 font-serif ">
+            <h1 className="text-3xl sm:text-5xl font-thin relative tracking-wide mb-2 font-serif ">
               <span className="font-sans">Nathan</span> MacDonald
             </h1>
-            <div className="flex items-center">
+            <div className="flex items-center flex-wrap text-sm sm:text-lg ">
               Lead Frontend Developer
               <HiPlus className="mx-2 h-3 w-3" /> Lead UX Designer
             </div>
           </div>
         </header>
-        <section className="flex space-x-16 mt-7  flex-grow">
-          <div className="w-[25rem] ">
+        <section className="flex sm:space-x-16 mt-7  flex-grow flex-wrap  overflow-auto">
+          <div className="w-full sm:w-[25rem] ">
             <Section title="ABOUT">
               <p className="text-gray-500 text-xs  text-justify">{aboutText}</p>
             </Section>
@@ -129,7 +127,6 @@ function App() {
                     i !== jobs.length - 1 && ""
                   )}
                 >
-                  {/* <div className="h-2 w-2 bg-orange-800 rounded-full shrink-0  mr-3 -translate-x-1"></div> */}
                   <HiChevronRight className="h-4 w-4 text-orange-800 rounded-full shrink-0  mr-1 -translate-x-1" />
 
                   <Job job={job} />
@@ -137,7 +134,7 @@ function App() {
               ))}
             </Section>
           </div>
-          <div className="mt-8  w-40">
+          <div className="mt-8  space-x-10 sm:space-x-0 sm:w-40 flex sm:flex-col">
             <Section title="SKILLS">
               <h5 className=" tracking-widest text-xs  mb-2 text-gray-700">
                 TECHNICAL
@@ -154,9 +151,11 @@ function App() {
             </Section>
 
             <Section title="EDUCATION">
-              <h4 className="text-sm  w-40  text-gray-600">
-                Bachelor of Computer Science, Minor in Business Administration
-                (CO-OP)
+              <h4 className="text-xs  w-40  text-gray-500">
+                <span className="font-medium text-gray-700">
+                  Bachelor of Computer Science
+                </span>
+                , Minor in Business Administration (CO-OP)
               </h4>
               <h5 className="text-xs text-orange-800/90 mt-1 font-medium">
                 University of New Brunswick
@@ -168,8 +167,8 @@ function App() {
           </div>
         </section>
 
-        <footer className="border-t border-orange-800/60 h-10 w-full text-xs py-4 text-gray-500 space-y-2 ">
-          <div className="flex items-center space-x-3 mb-4">
+        <footer className="border-t border-orange-800/60 w-full h-20 text-xs py-4 text-gray-500 space-y-2 ">
+          <div className="sm:flex items-center sm:space-x-3 mb-4 ">
             <span className="flex items-center ">
               <HiOutlineMail className="h-4 w-4 mr-1" /> natemacd97@gmail.com
             </span>
