1ffe0099598aaf3d9e2ff2c443cededb73c478ef
Nathan MacDonald <natemacd97@gmail.com>
2023-01-21 18:23:50 -0400

    converted images to web size, optimized for mobile

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,6 @@
-const Section = ({ title, children }: { title: string; children: React.ReactNode }) => {
+import { ReactNode } from 'react';
+
+const Section = ({ title, children }: { title: string; children: ReactNode }) => {
   return (
     <div className="mb-6">
       <h3 className="tracking-widest text-md font-medium mb-2 uppercase">{title}</h3>
